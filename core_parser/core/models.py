5ad3862b0b3e936d88febc169a3b66383811677e
MarqQ <marcos.w.queiroz@gmail.com>
2020-10-29 09:37:01 -0300

    Rewritten functions and logic, change DB fields, user interface implemented and spelling correction

--- core_parser/core/models.py
+++ core_parser/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class MainUrl(models.Model):
+    url = models.CharField(max_length=255)
+    file = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.url)
+
+
+class SecondaryUrl(models.Model):
+    final_link = models.CharField(max_length=255, null=True)
+    main_url = models.ForeignKey(MainUrl, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.final_link)
--- core_parser/core/views.py
+++ core_parser/core/views.py
@@ -1,27 +1,64 @@
 from django.shortcuts import render, redirect
 from bs4 import BeautifulSoup
 import requests
+import csv
+import uuid
+import json
+from .models import MainUrl
+from .models import SecondaryUrl
 
 
 def home(request):
-    return render(request, 'home.html')
+    url = MainUrl.objects.filter()
+
+    return render(request, 'home.html', {'url': url})
 
 
 def url_validation(request):
-    if request.method == 'POST':
-        url = request.POST['url']
-        source = requests.get(url).text
-        soup = BeautifulSoup(source, 'lxml')
+    url = request.POST.get('url')
+    data = requests.get(url).text
+    soup = BeautifulSoup(data, 'lxml')
 
-        for link in soup.find_all('a', href=True):
+    links = []
+    for link in soup.find_all('a'):
+        link_url = link.get('href')
 
-            first_char = link['href'][:4]
+        if link_url is not None and link_url.startswith('http'):
+            links.append(link_url)
+            # print(links)
+    create_file(request, links)
+    return redirect('/')
 
-            if "#" not in link['href']:
-                if first_char != 'http':
-                    final_link = url + link['href']
-                    print(final_link)
-                else:
-                    print(link['href'])
+
+def create_file(request, links):
+    filename = uuid.uuid4().hex[:32].upper()
+    # print(filename)
+
+    csv_file = open(filename+'.csv', 'w')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow([links])
+    csv_file.close()
+
+    with open(filename+'.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for line in csv_reader:
+            content = line
+            # print(content)
+        # f.writelines(links)
+        url = request.POST.get('url')
+        file = filename
+        url = MainUrl.objects.create(url=url, file=file)
+        url.save()
 
     return render(request, 'home.html')
+
+
+def get_all_links(url):
+    for link in url_validation(url):
+        get_all_links(link)
+
+
+# def fornecedoresDelete(request, id):
+#     fornecedor = Fornecedor.objects.get(id=id)
+#     fornecedor.delete()
+#     return redirect('fornecedores')
