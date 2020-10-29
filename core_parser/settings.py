063ecb8bf49c86b1dfd8bdb3b0bb9d55a25ade69
MarqQ <marcos.w.queiroz@gmail.com>
2020-10-29 09:38:24 -0300

    Database settings added

--- core_parser/settings.py
+++ core_parser/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'core_parser.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'url_collect',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
     }
 }
 
